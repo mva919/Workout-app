7c9c9874f299f38b1b2a3edf4dda4e46511e9080
mva919 <mva919@gmail.com>
2023-02-06 00:02:52 -0800

    Add multiselect to exercise search component

--- components/ExercisesSearch.js
+++ components/ExercisesSearch.js
@@ -2,6 +2,8 @@ import { useState, useEffect, useContext, useRef } from "react";
 import { ExercisesContext } from "../lib/ExercisesContext";
 import { BODY_PARTS, EXERCISE_CATEGORIES } from "../lib/constants";
 import { useOutsideClick } from "../lib/hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function ExercisesSearch({ handleExerciseClick, isPage }) {
   const exercises = useContext(ExercisesContext);
@@ -9,6 +11,7 @@ export default function ExercisesSearch({ handleExerciseClick, isPage }) {
   const [bodyPart, setBodyPart] = useState("");
   const [category, setCategory] = useState("");
   const [workoutList, setWorkoutList] = useState(exercises);
+  const [selectedWorkouts, setSelectedWorkouts] = useState([]);
 
   useEffect(() => {
     const filteredWorkouts = searchWorkoutList(
@@ -21,6 +24,21 @@ export default function ExercisesSearch({ handleExerciseClick, isPage }) {
     const filteredWorkouts = searchWorkoutList(
       e.target.value, bodyPart, category);
     setWorkoutList(filteredWorkouts);
+  };
+
+  const handleClick = (workout, index) => {
+    let oldArray = [...selectedWorkouts];
+    const itemIndex = oldArray.findIndex(selectedWorkout =>
+      selectedWorkout.index === index);
+
+    if (itemIndex !== -1) {
+      oldArray.splice(itemIndex, 1);
+      setSelectedWorkouts(oldArray);
+    } else {
+      const newWorkout = { workout: workout, index: index };
+      setSelectedWorkouts(oldWorkouts => [...oldWorkouts, newWorkout]);
+    }
+    console.log(selectedWorkouts);
   };
 
   const handleSubmit = (e) => {
@@ -50,8 +68,16 @@ export default function ExercisesSearch({ handleExerciseClick, isPage }) {
     <div className="text-center rounded py-4 my-2 h-full">
       {!isPage &&
         <div className="flex justify-end pr-6">
-          <button className="bg-indigo-500 text-white px-4 py-1 rounded">
-            Add Workout
+          <button
+            className="bg-indigo-500 text-white px-4 py-1 rounded
+          enabled:hover:bg-indigo-700 ease-in duration-100 disabled:opacity-75"
+            disabled={selectedWorkouts.length === 0}
+          >
+            {selectedWorkouts.length === 0 ? "Add Workout" :
+              selectedWorkouts.length > 1 ?
+                `Add ${selectedWorkouts.length} workouts` :
+                `Add ${selectedWorkouts.length} workout`
+            }
           </button>
         </div>
       }
@@ -85,21 +111,34 @@ export default function ExercisesSearch({ handleExerciseClick, isPage }) {
       <div className="h-full">
         <ul
           className={`flex flex-col items-start mx-auto w-1/2 shadow-sm rounded
-          overflow-y-scroll h-2/3 ${isPage ? "bg-slate-200" : "bg-slate-50"}`}
+          overflow-y-scroll h-2/3 ${isPage ? "bg-slate-200" : "bg-slate-50"}
+          `}
         >
           {/* ready state, displaying json data */}
-          {workoutList.length !== 0 ? workoutList.map((workout) => {
+          {workoutList.length !== 0 ? workoutList.map((workout, index) => {
             return (
               <li
                 key={workout.id}
-                className={`cursor-pointer flex flex-col items-start
-            justify-center px-4 py-1 border-b w-full 
-            hover:rounded hover:bg-slate-300
-            ${isPage ? "border-slate-300" : "border-slate-200"}`}
-                onClick={(e) => handleExerciseClick(workout)}
+                className={`cursor-pointer px-4 py-1 border-b w-full 
+                hover:rounded flex flex-row justify-between items-center
+                ${isPage ? "border-slate-300" : "border-slate-200"}
+                ${selectedWorkouts.find(elem => elem.index === index)
+                    !== undefined ?
+                    "bg-indigo-500 text-white hover:bg-indigo-700" :
+                    "hover:bg-slate-300"
+                  }
+                `
+                }
+                onClick={(e) => { if (!isPage) handleClick(workout, index) }}
               >
-                <p className="font-semibold">{workout.name}</p>
-                <p>{workout.muscle}</p>
+                <div className=" flex flex-col items-start justify-center">
+                  <p className="font-semibold">{workout.name}</p>
+                  <p>{workout.muscle}</p>
+                </div>
+                {selectedWorkouts.find(elem => elem.index === index) !==
+                  undefined && <FontAwesomeIcon icon={faCheck} />
+                }
+
               </li>
             );
           }) :
@@ -168,7 +207,6 @@ function DropdownButton({ dropdownOptions, buttonName,
           })}
         </div>
       }
-
     </div>
   );
 }
